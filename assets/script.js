1b8a1ec9a401dfc8c250be3c661aa2cbe82f8f5f
TK <tomking151183@gmail.com>
2022-11-09 19:23:38 +0000

    <Javascript has been added and debugged>

--- assets/script.js
+++ assets/script.js
@@ -1,19 +1,28 @@
 var topics = ['HTML', 'CSS', 'Git', 'Javascript'];
+var randomTopic = topics[Math.floor(Math.random() * topics.length)];
 
+function listTopics() {
 for (var x = 0; x < topics.length; x++) {
     console.log(topics[x])
+ }
 }
 
-/*
-if (topic === 'HTML') {
+function selectTopic() {
+ if (randomTopic === 'HTML') {
     console.log("Let's study HTML!");
-} else if (topic === 'CSS') {
+ } else if (randomTopic === 'CSS') {
     console.log("Let's study CSS!");
-} else if (topic === 'Git') {
+}  else if (randomTopic === 'Git') {
     console.log("Let's study Git!")
-} else if (topic === 'Javascript') {
+ } else if (randomTopic === 'Javascript') {
     console.log("Let's study Javascript");
-} else {
+ } else {
     console.log('Please try again');
+ }
 }
-*/
+
+console.log('Here are the topics we learned through Prework:');
+listTopics();
+
+console.log('Which topic should we strufy first?');
+selectTopic();
